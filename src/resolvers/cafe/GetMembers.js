c55de8edaa34a4fd4b700d765e8cce385a9c5450
xop141 <lowsens400@gmail.com>
2025-11-28 17:16:00 +0800

    get members

---
+++ src/resolvers/cafe/GetMembers.js
@@ -0,0 +1,27 @@
+
+import axios from "axios";
+export const GetMembers =async (req,res) =>{
+console.log(res,'res from front');
+const {cafeId,search_text} = req.body
+ try {
+    const response = await axios.get(
+      `/api/v2/cafe/${cafeId}/members/action/suggestMembers`,
+      {
+        baseURL: "https://api.icafecloud.com",
+        headers: {
+          Authorization: `Bearer ${process.env.NEXUS_KEY}`,
+        },
+        params: {
+          search_text: search_text
+        },
+      }
+    );
+
+    return res.json(response.data)
+  } catch (err) {
+    console.error("Error fetching members:", err.response?.data || err.message);
+    return null;
+  }
+
+
+}
