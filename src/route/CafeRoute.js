42b053a75e3a831efddf94366ea04341ee2254fd
xop141 <lowsens400@gmail.com>
2025-11-30 02:00:17 +0800

    asd

--- src/route/CafeRoute.js
+++ src/route/CafeRoute.js
@@ -4,7 +4,6 @@ import GetCafes from "../resolvers/cafe/GetCafes.js";
 import GetPcs from "../resolvers/cafe/GetPcs.js";
 import GetComments from "../resolvers/cafe/GetComments.js";
 import PostComment from "../resolvers/cafe/PostComment.js";
-import { GetMembers } from "../resolvers/cafe/GetMembers.js";
 import Topup from "../resolvers/cafe/Topup.js";
 const route = express.Router();
 
@@ -12,6 +11,5 @@ route.get("/", GetCafes);
 route.get("/pcs/:id", GetPcs);
 route.get("/comments/:id", GetComments);
 route.post("/commentPost/:id", ProfileMW, PostComment);
-route.get("/members", GetMembers);
 route.get("/members", Topup);
 export default route;
