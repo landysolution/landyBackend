b98af34f16d2ba205a20218f774417bde51b7945
xop141 <lowsens400@gmail.com>
2025-12-02 03:39:43 +0800

    asd

--- src/resolvers/member/TopUp.js
+++ src/resolvers/member/TopUp.js
@@ -1,5 +1,9 @@
 const TopUp = (req,res) =>{
-console.log(req.body);
 
+ console.log("=== QPay Callback Received ===");
+  console.log("Method:", req.method);
+  console.log("Headers:", req.headers);
+  console.log("Body:", req.body);
+  res.status(200).send("OK");
 }
 export default TopUp
