16d37cf6bc7389f9eb0f0ef273a8327f20d8bb45
Ubuntu <ubuntu@ip-172-31-40-73.ap-northeast-1.compute.internal>
2025-12-05 05:03:24 +0000

    sda

--- src/route/MemberRoute.js
+++ src/route/MemberRoute.js
@@ -4,13 +4,11 @@ import express from "express";
 import TopupMW from "../middleware/TopupMW.js";
 import CallBack from "../resolvers/member/CallBack.js";
 import postBank from "../resolvers/member/postBank.js";
-import getInvoice from '../resolvers/member/getInvoice.js'
 const route = express.Router();
 
 route.get("/", GetMember);
 route.post("/topup", TopupMW, TopUp);
 route.post("/notify", CallBack);
 route.post("/bank", postBank);
-route.post('/getInvoice',getInvoice)
 
 export default route;
