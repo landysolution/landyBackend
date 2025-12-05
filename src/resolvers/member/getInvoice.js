5688eb140b1b4a0b12119988a2ebefb2949f0242
Ubuntu <ubuntu@ip-172-31-40-73.ap-northeast-1.compute.internal>
2025-12-05 05:38:34 +0000

    sda

--- src/resolvers/member/getInvoice.js
+++ src/resolvers/member/getInvoice.js
@@ -2,21 +2,27 @@ import axios from "axios";
 
 const getInvocie = async (req, res) => {
   const { invoiceId } = req.body;
+
   try {
     const { data } = await axios.post(
-      "https://quickqr.qpay.mn/v2/payment/check",{
-Headers:{
-   Authorization: `Bearer ${process.env.qpayToken}`,
-}
+      "https://quickqr.qpay.mn/v2/payment/check",
+      {
+        invoice_id: invoiceId, // <-- Body MUST be second argument
       },
       {
-        invoice_id: invoiceId,
+        headers: {
+          Authorization: `Bearer ${process.env.qpayToken}`,
+          Accept: "application/json",
+        },
       }
     );
+
     return res.json(data);
+
   } catch (err) {
-    console.log(err);
-    return res.json(err);
+    console.log("QPay error:", err.response?.data || err.message);
+    return res.status(500).json(err.response?.data || err);
   }
 };
+
 export default getInvocie;
