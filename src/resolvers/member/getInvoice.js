4c618e116882703f7b2e5b87e5ce8ae1b593084b
Ubuntu <ubuntu@ip-172-31-40-73.ap-northeast-1.compute.internal>
2025-12-05 05:35:47 +0000

    sda

--- src/resolvers/member/getInvoice.js
+++ src/resolvers/member/getInvoice.js
@@ -4,7 +4,11 @@ const getInvocie = async (req, res) => {
   const { invoiceId } = req.body;
   try {
     const { data } = await axios.post(
-      "https://quickqr.qpay.mn/v2/payment/check",
+      "https://quickqr.qpay.mn/v2/payment/check",{
+Headers:{
+   Authorization: `Bearer ${process.env.qpayToken}`,
+}
+      },
       {
         invoice_id: invoiceId,
       }
