3ff4450b77efa0cd8d3077238f9fb483865563a1
xop141 <lowsens400@gmail.com>
2025-12-06 12:29:05 +0800

    sda

--- src/resolvers/cache/CafeCache.js
+++ src/resolvers/cache/CafeCache.js
@@ -6,26 +6,14 @@ let cafeCache = [];
 export const preloadCafeCache = async () => {
   try {
     let data = await RedisClient.get("cafe");
-
     if (data) {
       cafeCache = typeof data === "string" ? JSON.parse(data) : data;
-      console.log(`✅ Loaded ${cafeCache.length} cafes from Redis into memory`);
     } else {
       const cafeData = await cafeModel.find({});
       await RedisClient.set("cafe", JSON.stringify(cafeData));
       cafeCache = cafeData;
-      console.log(
-        `⚡ Loaded ${cafeCache.length} cafes from MongoDB into memory and Redis`
-      );
+      
     }
-
-
-
-
-
-
-
-
   } catch (err) {
     console.error("❌ Failed to load cafe cache:", err);
     cafeCache = [];
